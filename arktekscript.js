b7ba0846a21fd2242e04bb6267daa6e76cfa2c77
ArkTekLabs <123279630+ArkTekLabs@users.noreply.github.com>
2023-01-21 20:22:50 -0500

    Add files via upload

---
+++ arktekscript.js
@@ -0,0 +1,18 @@
+let arkpopOpen = document.querySelectorAll('.arkpop-open');
+let arkpopClose = document.querySelectorAll('.arkpop-close');
+
+arkpopOpen.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        let arkpop = document.querySelector(`.${this.dataset.arkpop}`);
+        arkpop.style.display = 'block';
+        arkpop.classList.add('fade-in');
+    });
+});
+
+arkpopClose.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        let arkpop = this.closest('.arkpop');
+        arkpop.style.display = 'none';
+        arkpop.classList.remove('fade-in');
+    });
+});
